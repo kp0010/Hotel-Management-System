3c1e72cc0fe911fccbedcf13c87e2c0de766cfde
kp0010 <122145240+kp0010@users.noreply.github.com>
2023-10-15 19:00:38 +0530

    Update

--- HotelManagementSystem/Reception.java
+++ HotelManagementSystem/Reception.java
@@ -14,79 +14,80 @@ public class Reception extends JFrame implements ActionListener {
     setLayout(null);
 
     newCustomer = new JButton("New Customer Forms");
-    newCustomer.setBounds(10, 30, 200, 30);
+    newCustomer.setBounds(22, 30, 200, 30);
     newCustomer.setBackground(Color.BLACK);
     newCustomer.setForeground(Color.WHITE);
     newCustomer.addActionListener(this);
     add(newCustomer);
 
     rooms = new JButton("Rooms");
-    rooms.setBounds(10, 70, 200, 30);
+    rooms.setBounds(22, 70, 200, 30);
     rooms.setBackground(Color.BLACK);
     rooms.setForeground(Color.WHITE);
     add(rooms);
-     
+
     department = new JButton("Department");
-    department.setBounds(10, 110, 200, 30);
+    department.setBounds(22, 110, 200, 30);
     department.setBackground(Color.BLACK);
     department.setForeground(Color.WHITE);
     add(department);
 
     allEmployee = new JButton("All Employees");
-    allEmployee.setBounds(10, 150, 200, 30);
+    allEmployee.setBounds(22, 150, 200, 30);
     allEmployee.setBackground(Color.BLACK);
     allEmployee.setForeground(Color.WHITE);
     add(allEmployee);
 
     customers = new JButton("Customer Info");
-    customers.setBounds(10, 190, 200, 30);
+    customers.setBounds(22, 190, 200, 30);
     customers.setBackground(Color.BLACK);
     customers.setForeground(Color.WHITE);
     add(customers);
 
     managerInfo = new JButton("Manager Info");
-    managerInfo.setBounds(10, 230, 200, 30);
+    managerInfo.setBounds(22, 230, 200, 30);
     managerInfo.setBackground(Color.BLACK);
     managerInfo.setForeground(Color.WHITE);
     add(managerInfo);
 
     checkout = new JButton("Checkout");
-    checkout.setBounds(10, 270, 200, 30);
+    checkout.setBounds(22, 270, 200, 30);
     checkout.setBackground(Color.BLACK);
     checkout.setForeground(Color.WHITE);
     add(checkout);
 
     update = new JButton("Update Status");
-    update.setBounds(10, 310, 200, 30);
+    update.setBounds(22, 310, 200, 30);
     update.setBackground(Color.BLACK);
     update.setForeground(Color.WHITE);
     update.addActionListener(this);
     add(update);
 
     roomStatus = new JButton("Update Room Status");
-    roomStatus.setBounds(10, 350, 200, 30);
+    roomStatus.setBounds(22, 350, 200, 30);
     roomStatus.setBackground(Color.BLACK);
     roomStatus.setForeground(Color.WHITE);
     roomStatus.addActionListener(this);
     add(roomStatus);
 
     pickup = new JButton("Pickup Service");
-    pickup.setBounds(10, 390, 200, 30);
+    pickup.setBounds(22, 390, 200, 30);
     pickup.setBackground(Color.BLACK);
     pickup.setForeground(Color.WHITE);
     add(pickup);
 
     searchRoom = new JButton("Search Room");
-    searchRoom.setBounds(10, 430, 200, 30);
+    searchRoom.setBounds(22, 430, 200, 30);
     searchRoom.setBackground(Color.BLACK);
     searchRoom.setForeground(Color.WHITE);
     searchRoom.addActionListener(this);
     add(searchRoom);
 
     logout = new JButton("Logout");
-    logout.setBounds(10, 470, 200, 30);
+    logout.setBounds(22, 470, 200, 30);
     logout.setBackground(Color.BLACK);
     logout.setForeground(Color.WHITE);
+    logout.addActionListener(this);
     add(logout);
 
     ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Assets/fourth.jpg"));
@@ -122,6 +123,9 @@ public class Reception extends JFrame implements ActionListener {
       } catch (SQLException e) {
         throw new RuntimeException(e);
       }
+      setVisible(false);
+    }
+    else if(ae.getSource() == logout) {
       setVisible(false);
     }
   }
